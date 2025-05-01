456a2015acf1daffeb4a88e75e1ec5dbade34576
lsh31124@gmail.com <lsh@naver.com>
2025-05-01 22:19:44 +0900

    feat: 기본 세팅 추가

---
+++ src/main/java/hello/hello/spring/controller/HelloController.java
@@ -0,0 +1,28 @@
+package hello.hello.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "test");
+        return "helle";
+    }
+    @GetMapping("helle-mve")
+    public String HelloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-templates";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+ name;
+    }
+
+
+}
